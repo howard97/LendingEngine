3246fddc9206c529c676942a57b5800ef91d408d
howard97 <howard12.sakala@gmail.com>
2021-07-27 20:59:17 +0200

    first commit

---
+++ src/main/java/com/takula/fund/lendingengine/LendingengineApplication.java
@@ -0,0 +1,13 @@
+package com.takula.fund.lendingengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendingengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendingengineApplication.class, args);
+	}
+
+}
